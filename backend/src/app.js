5fac84245370970e6670e7e2dc2af42e4686adc4
adam.metal <adam.metal@codecool.com>
2023-02-02 16:50:21 +0100

    add middlewares

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,15 +3,46 @@ const morgan = require('morgan');
 const cors = require('cors');
 const CardModel = require('./models/card.shema');
 const BidModel = require('./models/bid.schema');
-const { json } = require('express');
 
 const app = express();
 
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan('combined'));
 app.use(express.json());
 
-app.get('/api/cards', async (req, res) => {
+const time = (format) => {
+  return (req, res, next) => {
+    const now = new Date();
+
+    if (format === 'iso') {
+      console.log(now.toISOString());
+    } else if (format === 'simple') {
+      console.log(now.toDateString());
+    } else {
+      console.log('lol noob');
+    }
+
+    next();
+  }
+}
+
+app.use(time('iso'));
+
+const getBidById = (id) => BidModel.findById(id);
+
+app.use('/api/bids/:id', async (req, res, next) => {
+  const bid = await getBidById(req.params.id);
+
+  if (!bid) {
+    res.status(404).end();
+    return;
+  }
+
+  req.bid = bid;
+  next();
+});
+
+app.get('/api/cards', async (req, res, next) => {
   const cards = await CardModel.find();
   res.json(cards);
 });
@@ -26,11 +57,9 @@ app.get('/api/bids', async (req, res) => {
   res.json(bids);
 });
 
-const getBidById = (id) => BidModel.findById(id);
 
-app.get('/api/bids/:id', async (req, res) => {
-  const bid = await getBidById(req.params.id);
-  res.json(bid);
+app.get('/api/bids/:id', (req, res) => {
+  res.json(req.bid);
 });
 
 app.post('/api/bids', async (req, res) => {
